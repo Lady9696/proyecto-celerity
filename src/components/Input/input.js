cf9228e26abf486a9c1a13d37ec670f9a0195382
Lady9696 <leidysuarez55@gmail.com>
2024-01-11 09:33:25 -0500

    proyecto finalizado

--- src/components/Input/input.js
+++ src/components/Input/input.js
@@ -1,7 +1,8 @@
 import './input.css'
 const Input = () => {
   return <>
-  <input className='input-meters' type="text" />
+  <input className='input-meters' placeholder='0' type="text"
+   />
   </>
 }
 export default Input
